07fe356f76d38794698cf1b6c53fbf5c0f704585
Swapnil Kesur <kesurswapnil@gmail.com>
2017-06-15 17:06:30 +0530

    Validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,6 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :article
   
-  validates :body, length: { minimum: 5, maximum: 500 }
+  validates :body, length: { minimum: 0, maximum: 2000 }
   
 end
