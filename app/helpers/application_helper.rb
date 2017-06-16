96b6c9af3888edbb01a34df35a1f1b15a2f5c897
Swapnil Kesur <kesurswapnil@gmail.com>
2017-06-16 16:57:40 +0530

    Update application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  	def gravatar_for(user)
+		gravator_id = Digest::MD5::hexdigest(user.email.downcase)
+		gravatar_url = "htpps://secure.gravator.com/avatar/#{gravator_id}"
+		image_tag(gravatar_url, alt:user.username, class: "img-circle")
+	end
+
 end
